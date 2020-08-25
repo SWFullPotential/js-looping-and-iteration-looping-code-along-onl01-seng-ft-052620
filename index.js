27b2af5c54483f947c8926446835772df53613c5
SWFullPotential <swfullpotential@gmail.com>
2020-08-25 13:42:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let array = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+
+function countDown(num) {
+for (let i = 0; i <= num; num--) {
+    console.log(num)
+    }
+}
